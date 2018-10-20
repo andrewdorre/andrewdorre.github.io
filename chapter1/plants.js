4651ef7c8f4a2b19646edc8a9df0c49a0ed76cda
Andrew Dorre <andrewdorre@gmail.com>
2018-10-20 11:35:27 -0700

    asdf

---
+++ chapter1/plants.js
@@ -0,0 +1,16 @@
+/*  
+    JavaScript 6th Edition
+    Chapter 1
+    Chater case
+
+    Tinley Xeriscapes
+    Author: Andrew Dorre
+    Date: 10-20-2018
+
+    Filename: plants.js
+
+*/
+
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
