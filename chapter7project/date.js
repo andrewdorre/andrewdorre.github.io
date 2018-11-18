fc5bff0f53ab1f98dc7fe6b71fdc48ef3e897bfd
Andrew Dorre <andrewdorre@gmail.com>
2018-11-18 11:09:23 -0700

    ch7 progress

--- chapter7project/date.js
+++ chapter7project/date.js
@@ -1,10 +1,31 @@
 "use strict";
 
-function date() {
-    var today = new Date();
-    var yesterday = new Date();
-    document.getElementById("today").innerHTML = "Today: " + today;
-    document.getElementById("yesterday").innerHTML = "Yesterday: " + yesterday + "Millis: " + yesterday.valueOf();
-    document.getElementById("subtraction").innerHTML = "Today minus Yesterday: " + today - yesterday;
+var thirtyOne = ["January", "March", "May", "July", "August", "October", "December"];
+var thirty = ["April", "June", "September", "November"];
 
+var today = new Date();
+var newDate = new Date();
+var userDate = new Date();
+var solution;
+var year;
+var month;
+var day;
+var singleDay = 1000 * 60 * 60 * 24;
+
+function date() {
+    
+    document.getElementById("today").innerHTML = "Today: " + today;
+    /* document.getElementById("yesterday").innerHTML = "Yesterday: " + yesterday + "Millis: " + yesterday.valueOf();
+    document.getElementById("subtraction").innerHTML = "Today minus Yesterday: " + today - yesterday; */
+    year = document.getElementById("yearSubmit").value;
+    month = document.getElementById("monthSubmit").value;
+    day = document.getElementById("daySubmit").value;
+    userDate.setFullYear(year, month - 1, day);
+    document.getElementById("userDate").innerHTML = "Date you entered: " + userDate;
+    solution = today.getTime() - userDate.getTime();
+    document.getElementById("subtraction").innerHTML = "Current date minus your date: " + Math.round(solution / singleDay);
+
+}
+function leapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
