5a6a1729214c010379ec9ac5a67b0da62a484192
Andrew Dorre <andrewdorre@gmail.com>
2018-11-22 12:47:45 -0700

    Update string.js

--- chapter8project/string.js
+++ chapter8project/string.js
@@ -10,33 +10,44 @@
       Filename: string.js */
 "use strict";
 
+// Array to store the checkbox values
 var pizzaToppings = [];
 
+// function to add topping values to the array
 function addToppings(event) {
     if (event === undefined) {
         event = window.event;
-     }
-     var callerElement = event.target || event.srcElement;
-     var toppings = callerElement.value;
-     if (callerElement.checked) {
-         pizzaToppings.push(toppings);
-         var newTopping = document.createElement("li");
-         newTopping.innerHTML = toppings;
-         document.getElementById("toppingslist").appendChild(newTopping);
-         //document.getElementById("toppingsdiv").style.display.block;
-     }
-     else {
-         var toppingsItems = document.querySelectorAll("#toppingslist li")
+    }
+    // "callerElement" stores the variable of the checkbox that is checked
+    var callerElement = event.target || event.srcElement;
+    // "toppings" holds the value of the checkbox, which is the string
+    var toppings = callerElement.value;
+    // If the element is checked, "push" the string value of that checkbox to the pizzaToppings array
+    if (callerElement.checked) {
+        pizzaToppings.push(toppings);
+    // Also, like in the book example, I use an unordered list, and add the array nodes to the list
+        var newTopping = document.createElement("li");
+        newTopping.innerHTML = toppings;
+        document.getElementById("toppingslist").appendChild(newTopping);         
+    }
+    // if a box is unchecked, remove it from the array and "splice" the array back together, and remove the
+    // list item from the html
+    else {
+    // create a variable that holds all of the li elements in the toppingslist list
+        var toppingsItems = document.querySelectorAll("#toppingslist li")
+    // run a for loop to check each element and remove it if it is unchecked
         for (var i = 0; i < pizzaToppings.length; i++) {
-           if (toppingsItems[i].innerHTML === toppings) {
-              pizzaToppings.splice(i, 1);
-              toppingsItems[i].parentNode.removeChild(toppingsItems[i]);
-              break;
-           }
+            if (toppingsItems[i].innerHTML === toppings) {
+                pizzaToppings.splice(i, 1);
+                toppingsItems[i].parentNode.removeChild(toppingsItems[i]);
+                break;
+            }
         }
-     }
+    }
 }
 
+// Create event listeners on the checkboxes, so that when they are clicked, it triggers
+// the javascript
 function createEventListeners() {
     var pizToppings = document.getElementsByName("toppings");
     if (pizToppings[0].addEventListener) {
@@ -47,11 +58,12 @@ function createEventListeners() {
         for (var i = 0; i < pizToppings.length; i++) {
             pizToppings[i].attachEvent("onchange", addToppings);
         }
-    }
-    
+    }    
 }
+
 if (window.addEventListener) {
     window.addEventListener("load", createEventListeners, false);
-} else if (window.attachEvent) {
+} 
+else if (window.attachEvent) {
     window.attachEvent("onload", createEventListeners);
 }
