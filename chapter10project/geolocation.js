8714adf9d2c6f207109a589beabe36a8eba37f4e
Andrew Dorre <andrewdorre@gmail.com>
2018-11-24 16:13:56 -0700

    ch10 tests

--- chapter10project/geolocation.js
+++ chapter10project/geolocation.js
@@ -12,54 +12,43 @@
 */
 "use strict";
 
-/* function getLocation() {           
+var waitForUser;
+
+function geoTest() {
+    waitForUser = setTimeout(fail, 10000);
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        
-    } 
-    else {
-        document.getElementById("location").innerHTML = "Location info is not support in this browser or browser version.";
+       navigator.geolocation.getCurrentPosition(initialize, fail, {timeout: 10000});
+    } else {
+       fail();
     }
-} */
+ }
+
 function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    clearTimeout(waitForUser);
+    //var latlon = position.coords.latitude + "," + position.coords.longitude;
+    var currPosLat = position.coords.latitude;
+    var currPosLng = position.coords.longitude;
     var mapOptions = {
-        center: new google.maps.LatLng(latlon),
-        zoom: 11
+       center: new google.maps.LatLng(currPosLat, currPosLng),
+       zoom: 11
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-function createMap(showPosition) {
-    
-    //var latitude = navigator.geolocation.getCurrentPosition(coords.latitude);
-        //var longitude = position.coords.longitude;
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-            /* var lat = navigator.geolocation.getCurrentPosition(position.coords.latitude);
-            var long = position.coords.longitude; */
-            
-        } 
-        else {
-            document.getElementById("location").innerHTML = "Location info is not support in this browser or browser version.";
-        }
-        var mapOptions = {
-            center: new google.maps.LatLng(latlon),
-            zoom: 11
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
 }
 
 function createEventListeners() {
     var buttonClick = document.getElementById("button");
     if (buttonClick.addEventListener) {
-        buttonClick.addEventListener("click", createMap, false);
+        buttonClick.addEventListener("click", geoTest, false);
     } else if (buttonClick.attachEvent) {
-        buttonClick.attachEvent("onclick", createMap);
+        buttonClick.attachEvent("onclick", geoTest);
     }
 }
+window.initMap = function() {};
 
 if (window.addEventListener) {
     window.addEventListener("load", createEventListeners, false);
+    window.initMap = function() {};
 } else if (window.attachEvent) {
     window.attachEvent("onload", createEventListeners);
 }
