f3bf8ab8ac9fd776bdd1ee2319a8595abc58d3bb
Andrew Dorre <andrewdorre@gmail.com>
2018-11-28 13:34:49 -0700

    Ch11 It's WORKING!!!!

--- chapter11project/newsJSONP.js
+++ chapter11project/newsJSONP.js
@@ -12,24 +12,59 @@
 
     Powered by https://newsapi.org
 */
+// This saved my life: https://code-maven.com/ajax-request-for-json-data
 "use strict";
+const Http = new XMLHttpRequest();
+/* const url = 'https://api.iextrading.com/1.0/stock/fb/quote'; */
 
 var url = 'https://newsapi.org/v2/top-headlines?' +
     'country=us&' +
     'apiKey=5cb35fea7dfd4098abd2498570bdcfb9';
-var i, x, test = "";
+var IEX = 'https://api.iextrading.com/1.0?symbols=SNAP,fb';
+/* var i, x, test = "";
 var myObj;
 
-var req = new Request(url);
+var req = new Request(url); */
 
-function testFunc() {
-    fetch(req)
+function testFunc(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch (err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    /* fetch(url)
+        .then(data => { return data.json() })
+        .then(res => { console.log(res) }) */
+    
+/*     $.ajax({
+        type: 'GET',
+        url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=5cb35fea7dfd4098abd2498570bdcfb9',        
+        success: function (feed) {
+            $("<h1>").text(feed.articles).appendTo("body");
+        },
+        dataType: 'jsonp'
+
+    }); */
+    /* fetch(req)
         .then(function (response) {
-            console.log(response.json());
-            /* myObj = response.json(); */
-        })
-        
-        /* document.getElementById("test2").innerHTML = x; */
+            console.log(response.json()); */
+            
+            //myObj = response.json();
+        /* }) */
+    /* var myJSON = JSON.parse(object);
+    document.getElementById("test2").innerHTML = myJSON.articles["0"].title; */
+    /* document.getElementById("test2").innerHTML = response.json(); */
             //console.log(response.json());
             //console.log(articles)                           
         
@@ -37,8 +72,6 @@ function testFunc() {
             
         var object = response.json();
         var myJSON = JSON.stringify(object);
-
-
         document.getElementById("test2").innerHTML = myJSON;
         }) */
     //var myJSON = JSON.stringify(object.articles[0]);
@@ -50,11 +83,24 @@ function testFunc() {
     alert(myObj["object"]["articles"]["0"]["author"]); */
     
 }
-function openPromise() {
+
+testFunc(url, function (data) {
+    document.getElementById("test2").innerHTML = data["status"];
+
+    var html = "<h2>" + data["status"] + "</h2>";
+    html += "<ul>";
+    for (var i = 0; i < data["articles"].length; i++) {
+        html += '<li>' + data["articles"][i]["title"] + "</li>";
+    }
+    html += "</ul>";
+    document.getElementById("newstest").innerHTML = html;
+
+});
+/* function openPromise() {
     var json = JSON.parse(object);
 
     alert(json["articles"]);
-}
+} */
 
 function createEventListeners() {
     var buttonClick = document.getElementById("button");
